7199f46a4d8dbd788bb79b0f8622c4958e8c4cb9
unknown <landon3388@gmail.com>
2022-11-04 00:33:51 -0500

    ggggdd

--- ormconfig.js
+++ ormconfig.js
@@ -1,13 +1,12 @@
 module.exports = {
    "type": "oracle",
-   "host": "10.5.10.10",
-<<<<<<< HEAD
+
    "port": 1521,
    "username": "330496500",
-=======
+
    "port":  1521,
    "username": "tramite",
->>>>>>> fa6be70bd7f02e48d2da1866dbb83f0b541f5ed8
+
    "password": "123456",
    // "host": process.env.DB_HOST,
    // "port": process.env.DB_PORT,
@@ -15,18 +14,15 @@ module.exports = {
    // "password": process.env.DB_PASSWORD,
    // "database": "TRAMITE",
    // "database": "DIEBDDE",
-<<<<<<< HEAD
+
    "sid": "DIEBDDE",
-=======
-<<<<<<< HEAD
+
+
    "connectString": "10.5.10.10/DIEBDDE",
    "sid": "DIEBDDE",
    "synchronize": true,
-=======
    "sid": "xe",
->>>>>>> 8cc97c234bb382f8d610c3fdbd2783b4688f3608
    "synchronize": false,
->>>>>>> fa6be70bd7f02e48d2da1866dbb83f0b541f5ed8
    "logging": false,
    "entities": [
       "dist/entity/**/*{.ts,.js}"
