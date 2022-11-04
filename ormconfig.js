ae22e75bb31880cecf081f69d2f62584c22d31c7
rolando-d3v <landon3388@gmail.com>
2022-11-03 19:57:25 -0500

    rolando

--- ormconfig.js
+++ ormconfig.js
@@ -1,8 +1,8 @@
 module.exports = {
    "type": "oracle",
    "host": "10.5.10.10",
-   "port": "1521",
-   "username": "tramite",
+   "port": 1521,
+   "username": "330496500",
    "password": "123456",
    // "host": process.env.DB_HOST,
    // "port": process.env.DB_PORT,
@@ -10,21 +10,22 @@ module.exports = {
    // "password": process.env.DB_PASSWORD,
    // "database": "TRAMITE",
    // "database": "DIEBDDE",
+   "connectString": "10.5.10.10/DIEBDDE",
    "sid": "DIEBDDE",
    "synchronize": true,
-   // "logging": false,
-   // "entities": [
-   //    "dist/entity/**/*{.ts,.js}"
-   // ],
-   // "migrations": [
-   //    "dist/migration/**/*{.ts,.js}"
-   // ],
-   // "subscribers": [
-   //    "dist/subscriber/**/*{.ts,.js}"
-   // ],
-   // "cli": {
-   //    "entitiesDir": "dist/entity",
-   //    "migrationsDir": "dist/migration",
-   //    "subscribersDir": "dist/subscriber"
-   // }
+   "logging": false,
+   "entities": [
+      "dist/entity/**/*{.ts,.js}"
+   ],
+   "migrations": [
+      "dist/migration/**/*{.ts,.js}"
+   ],
+   "subscribers": [
+      "dist/subscriber/**/*{.ts,.js}"
+   ],
+   "cli": {
+      "entitiesDir": "dist/entity",
+      "migrationsDir": "dist/migration",
+      "subscribersDir": "dist/subscriber"
+   }
 }
